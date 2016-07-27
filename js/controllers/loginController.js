7583ff0fa38b6db72f17e50786480aa897ecb204
Malorie Kramer <mkramer2089@gmail.com>
2016-07-27 10:14:42 -0400

    local storage cont.

--- js/controllers/loginController.js
+++ js/controllers/loginController.js
@@ -7,7 +7,7 @@
        var vm = this;
        vm.showAlert = false;
 
-       if(API.getToken() !== null)
+       if(back.getToken() !== null)
        {
         $state.go('admin');
        }
@@ -19,8 +19,8 @@
           console.log(results);
           if(results.data.data[0])
           {
-            API.saveToken(results.data.data[0].token);
-            API.saveUserId(results.data.data[0].id);
+            back.saveToken(results.data.data[0].token);
+            back.saveUserId(results.data.data[0].id);
             vm.showAlert = false;
             $state.go('admin');
           }
