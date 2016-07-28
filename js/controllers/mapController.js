b4c63db6d07aad33b86697c69f1ad5fa90343a0f
Jared <jcknueven@gmail.com>
2016-07-28 10:53:54 -0400

    map added with style

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -1,22 +1,21 @@
-// (function() {
-//     'use strict';
+(function() {
+    'use strict';
     
-//     angular
-//     .module('parkalot')
-//     .controller('mapController', function($state,back,API) {
-//        var vm = this;
-       
-//        var mymap = L.map('mapid').setView([39.1031, -0.09], 13);
+    angular
+    .module('parkalot')
+    .controller('mapController', function($state,back) {
+       var vm = this;
 
-//        L.tileLayer('https://a.tiles.mapbox.com/v4/jcknueven.0pa4k9a3/page.html?access_token=pk.eyJ1IjoiamNrbnVldmVuIiwiYSI6ImNpcjUxcXJ2eTAxbzNmbm5yMW1naGE3NWoifQ.YhmcfQV-iBNW-rj3XLNzaw#15/39.1025/-84.5197', 
-//        {
-//     	attribution: '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, <a href="http://mapbox.com">Mapbox</a>',
-//    		maxZoom: 18,
-//     	id: 'jcknueven.0pa4k9a3',
-//     	accessToken: 'pk.eyJ1IjoiamNrbnVldmVuIiwiYSI6ImNpcjUxcXJ2eTAxbzNmbm5yMW1naGE3NWoifQ.YhmcfQV-iBNW-rj3XLNzaw#15/39.1025/-84.5197'
-// 		}).addTo(mymap);
+      var mymap = L.map('mapid').setView([39.1031, -84.5120], 13);
+
+      L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', 
+       {
+    	//attribution: '<a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, <a href="http://mapbox.com">Mapbox</a>',
+   		maxZoom: 18,
+    	id: 'jcknueven.0pa4k9a3',
+    	accessToken: 'pk.eyJ1IjoiamNrbnVldmVuIiwiYSI6ImNpcjUxcXJ2eTAxbzNmbm5yMW1naGE3NWoifQ.YhmcfQV-iBNW-rj3XLNzaw#15/39.1025/-84.5197'
+		}).addTo(mymap);
       
       
-      
-//     });
-// })();
+    });
+})();
