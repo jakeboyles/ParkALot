b0cb6492eda1c2cad456b4b0e998b09ec574f192
Malorie Kramer <mkramer2089@gmail.com>
2016-08-01 16:07:19 -0400

    ugh mobile menu

--- js/controllers/mobileMenuController.js
+++ js/controllers/mobileMenuController.js
@@ -1,12 +1,1 @@
-// (function () {
-// 	'use strict';
-// 	angular
-// 		.module('parkalot').factory('back', function($http,Backand) {
-
-// 			.mobile.on("click", function(){
-// 			  this.toggleClass("open");
-// 			.menu.toggleClass("active");
-// 			});
-
-// 	})		
-// })();			
+			
