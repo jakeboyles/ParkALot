8655b51d963fa7ba966f6298316a5c0a068848d3
Malorie Kramer <mkramer2089@gmail.com>
2016-08-08 13:25:16 -0400

    things

--- js/controllers/signUpController.js
+++ js/controllers/signUpController.js
@@ -6,16 +6,27 @@ angular.module("parkalot").controller("signUpController", function($state,back) 
 	var vm = this;
 
 	vm.submit = function(){
-		var createData = back.postData(vm.form);
+		var token = randomString(64, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
+		var createData = back.postData(vm.form,token);
 		
 		 createData.then(function(response){
 		 	back.saveUserInfo(response.data.__metadata.id);
-		 	back.saveToken(response.data.token);
-		 	// console.log(response);
-		 	 $state.go('userPref');
+		 	back.saveToken(token);
+		 	console.log(token);
+		 	//console.log(response);
+		 	$state.go('userPref');
 
 		})
        }
+
+       //create random string for token to be returned
+			function randomString(length,charac) {
+				var results = '';
+
+				for(var i = length; i > 0; --i) results += charac[Math.floor(Math.random() * charac.length)];
+
+				return results;
+			} 	
       
     });
 })();
