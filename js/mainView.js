45cfeda2595bf8b63603bd42a970916d24021813
Jared <jcknueven@gmail.com>
2016-08-08 12:57:15 -0400

    new hover/touch

--- js/mainView.js
+++ js/mainView.js
@@ -2,8 +2,9 @@
 'use strict';
 
 angular.module('parkalot', ['ui.router', 'backand','ngMaterial'])
-.config(function($stateProvider, $urlRouterProvider, $locationProvider, BackandProvider){
-
+.config(function($stateProvider, $urlRouterProvider, $locationProvider, BackandProvider, $mdGestureProvider){
+	
+	$mdGestureProvider.skipClickHijack();
 	BackandProvider.setAppName('parkalot1');
     BackandProvider.setSignUpToken('e00c2312-a29b-42fe-8969-5d25082ba74b');
     BackandProvider.setAnonymousToken('26c9ccdc-78a0-45b5-b13f-159b6fbe4930');
