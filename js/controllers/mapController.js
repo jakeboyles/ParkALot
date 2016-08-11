91486adf6b7e3c6b0cfd423962df64fe65a58028
Malorie Kramer <mkramer2089@gmail.com>
2016-08-11 16:53:02 -0400

    some really awesome things

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -6,17 +6,16 @@
       var vm = this;
 
       
-
+      //function to check that user has token and token prefill map form w/prefs
       if(back.getToken() !== null){
+        
         var preFill = back.userGet(back.getToken());
 
-       preFill.then(function(data){
-        console.log(data);
-       vm.form.distance = data.data.data[0].distance;
-       vm.form.price = data.data.data[0].price;
-
-       });
-
+         preFill.then(function(data){
+          console.log(data);
+         vm.form.distance = data.data.data[0].distance;
+         vm.form.price = data.data.data[0].price;
+         });
       }
      
 
